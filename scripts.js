e5b5090305122871754e83ff4b6028fa07b0e27f
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-20 17:20:00 -0500

    git commit -m "added code to attempt random div colors"

--- scripts.js
+++ scripts.js
@@ -11,3 +11,17 @@ var div = document.createElement("div");
     div.innerHTML = "1";
     document.body.appendChild(div);
 });
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeDivColor() {
+    var div = document.getElementById("div");
+    var color = getRandomColor();
+    div.style.backgroundColor = color;
+};
