4b77f1a21f70159119314daa2045af8d636e2a7d
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-20 16:05:33 -0500

    added div box

--- scripts.js
+++ scripts.js
@@ -3,4 +3,11 @@ var button = document.createElement("button");
     button.innerHTML = "";
     document.body.appendChild(button);
 //DIVs (emptyTiles) onClick function//
+var div = document.createElement("div");
+    div.style.width = "10em";
+    div.style.height = "10em";
+    div.style.background = "black";
+    div.style.color = "white";
+    div.innerHTML = "1";
+    document.body.appendChild(div);
 
