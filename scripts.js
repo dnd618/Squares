d6711a321128771ec53bfdb0e673bf6374d1afa9
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-20 14:56:48 -0500

    i don't know what to call these changes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-function button_click(color) {
-     document.getElementById("box").style.backgroundColor = color;
-     document.getElementById("box1").style.backgroundColor = color;
-};
-('body').on('click', '.change-color', function(){
-    var color = $(this).data('color');
-    $('.global-class-'+color).css('background-color', color);
-});
+//Button
+var button = document.createElement("button");
+    button.innerHTML = "";
+    document.body.appendChild(button);
+//DIVs (emptyTiles) onClick function//
+
