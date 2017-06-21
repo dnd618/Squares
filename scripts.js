983d70170e648ee94923979af9b4631cfd7684cb
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-21 17:08:38 -0500

    added notes on alert button

--- scripts.js
+++ scripts.js
@@ -41,4 +41,8 @@ function getRandomColor() {
 };
 //Make odd & even DIVs disappear
 
+//Alert hint
+if (box is even & it's not the last element then remove next element)
+else if (the previous element is not the button then remove previous element)
+else [ the previous button is not a button, then alert]
 
