800a06aaae6498602a3a7fc2cc118b6d09065255
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-22 09:05:54 -0500

    final commit

--- scripts.js
+++ scripts.js
@@ -1,51 +1,46 @@
-//Button
-var button = document.createElement("button");
-    button.innerHTML = "";
-    var counter = 0;
-    document.body.appendChild(button);
-        button.addEventListener ("click", function() {
-var div = document.createElement("div");
-    div.style.width = "10em";
-    div.style.height = "10em";
-    div.style.background = "black";
-    div.style.color = "white";
-    div.style.textAlign = "center";
-    div.innerHTML = "";
-    div.style.padding = "2em;"
-    counter++;
-    div.id = counter;
-    document.body.appendChild(div);
-//Naming DIV classes
-var d = document.getElementById("div");
-div.className += "someDivs";
-div.addEventListener('click', function(){
-    div.style.backgroundColor = getRandomColor();
-    });
-
-// //# on DIV with mouseover
-    div.addEventListener('mouseover', function(){
-        div.innerText = div.id;
-    div.addEventListener('mouseleave', function(){
-        div.innerText = ""
+var i = -1
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.createElement('button');
+    button.innerText = 'Click Me!'
+    document.body.appendChild(button)
+    var container = document.createElement('div')
+    document.body.appendChild(container)
+    button.addEventListener("click", function () {
+        var square = document.createElement('div');
+        square.className = 'squares';
+        i++;
+        square.id = [i]
+        square.addEventListener('mouseover', function () {
+            square.innerText = square.id;
+        });
+        square.addEventListener('mouseout', function () {
+            square.innerText = '';
+        });
+        square.addEventListener('click', function () {
+            var backColor = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"]
+            var rand = backColor[Math.floor(Math.random() * backColor.length)];
+            console.log(rand)
+            square.style.backgroundColor = (rand);
+        })
+        square.addEventListener('dblclick', function () {
+            if (square.id % 2 !== 0 && square == container.firstChild) {
+                alert('Element does not extist')
+                console.log('odd work')
+            }
+            else if (square.id % 2 !== 0) {
+                square.previousElementSibling.remove()
+                console.log('odd work')
+            }
+            else if (square == container.lastChild) {
+                alert('Element does not exist!')
+                console.log('lst child work')
+            }
+            else if (square.id % 2 == 0) {
+                square.nextElementSibling.remove()
+                console.log('even work')
+            }
+        })
+        container.appendChild(square)
+        var boxCount = document.querySelectorAll(container > square).length;
     });
 });
-});
-//Function for div random colors
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-};
-//Make odd & even DIVs disappear
-//
-div.nextElementSibling
-previousElementSibling
-.remove ();
-//Alert hints//
-if (box is even & it's not the last element then remove next element)
-else if (the previous element is not the button then remove previous element)
-else [ the previous button is not a button, then alert]
-
